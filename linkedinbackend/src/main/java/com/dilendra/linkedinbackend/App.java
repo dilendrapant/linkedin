bc6852e1eeab90711aea0d6361931405c9675070
Dilendra Pant <pant.dilendra@gmail.com>
2018-01-27 17:30:48 +0530

    linkedin setup completed

---
+++ linkedinbackend/src/main/java/com/dilendra/linkedinbackend/App.java
@@ -0,0 +1,13 @@
+package com.dilendra.linkedinbackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
